90d015dcb3cd1a49ec15fbca643cc5d5a3b59493
daniharo <daniharopadul@gmail.com>
2021-03-09 16:56:39 +0100

    Primeros archivos añadidos

---
+++ CalculadoraExtremos.java
@@ -0,0 +1,6 @@
+public class CalculadoraExtremos implements Observer {
+
+	private int max;
+	private int min;
+
+}
---
+++ Notas.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Notas extends Observable {
+
+	ArrayList<Nota> calificaciones;
+
+	public ArrayList<Nota> getState() {
+		// TODO - implement Notas.getState
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param calificaciones
+	 */
+	public void setState(ArrayList<Nota> calificaciones) {
+		// TODO - implement Notas.setState
+		throw new UnsupportedOperationException();
+	}
+
+}
